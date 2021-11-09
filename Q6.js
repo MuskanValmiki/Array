3a4fead11a35c0fcccac375cc8aaf80a1807a09c
Muskan Valmikee <83203071+MuskanValmiki@users.noreply.github.com>
2021-11-09 12:42:03 +0530

    Update Q6.js

--- Q6.js
+++ Q6.js
@@ -1,9 +1,22 @@
-var marks = [ [78, 76, 94, 86, 88], [91, 71, 98, 65, 76], [95, 45, 78, 52, 49] ];
-var sum = 0;
-for (var i of  marks ){
-    for(var j of i){
-        sum+=j
+var x=10;
+var y=12;
+var z=90;
+if(x>y){
+    if(x<z){
+       x=y
+    }
+    else{
+        m=z
     }
 }
-console.log(sum);
-// sum of marks
+else{
+    if(y>z){
+        m=y
+    }
+    else{
+        m=x
+    }
+}
+console.log(m)
+        
+// find the second max
